9ab897e83d25430573f3ddf46431b8d2d917dcf9
Name-Expunged <91637275+Name-Expunged@users.noreply.github.com>
2021-10-06 21:49:22 -0400

    Add files via upload

---
+++ Jacob_Nugent_ICE1/CardTrick.java
@@ -0,0 +1,15 @@
+
+package ca.sheridancollege.week3.softwarefundamentals.ice1.Friday;
+
+/**
+ *Jacob Nugent
+ *991608217
+ */
+public class CardTrick {
+    public static void main(String[] args)
+    {
+       Card card = new Card();
+       Card cooler card = new Card();
+    }
+    
+}
